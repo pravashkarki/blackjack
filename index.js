67366880e1e5f5119c883e6aead15ef6a5c2c7c7
Pravash Karki <pravash.karki@heraldcollege.edu.np>
2023-07-17 14:55:17 +0545

    fix minor bug

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-console.log(cards)
-
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    let randomNumer = Math.floor(Math.random() * 13) + 1
     if (randomNumer > 10) {
         return 10
     } else if (randomNumer === 1) {
@@ -26,8 +24,6 @@ function startGame() {
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
-    // Generate two random numbes
-    // Re-assign the cards and sum variables so that the game can start
     renderGame()
 }
 
@@ -36,7 +32,7 @@ function renderGame() {
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
-    
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -52,10 +48,12 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
 }
-
-
